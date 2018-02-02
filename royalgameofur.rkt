3fa7b9aa8c683bd052ed50d724729e4186f7ecc4
Nick Scheuring <scheuring.n@husky.neu.edu>
2018-02-02 12:40:00 -0500

    just paste the gameboard, no need to build it

--- royalgameofur.rkt
+++ royalgameofur.rkt
@@ -5,17 +5,13 @@
 (define HEIGHT 1400)
 (define WIDTH 800)
 (define SCENE (empty-scene HEIGHT WIDTH))
-
-(define magic-tile (square 20 "solid" "blue") )
-(define paddle (circle 20 "solid" "blue"))
-(define vertical-line (rectangle 10 HEIGHT "solid" "black"))
-(define game-board-bg (rectangle (/ HEIGHT 2) (/ WIDTH 2) "solid" "blue"))
+(define BOARD .pasted-image)
 
 
 
-(define game-board (place-image vertical-line 0 0 game-board-bg))
 
-(define (draw-state input) (place-image game-board (/ HEIGHT 2) (/ WIDTH 2) SCENE))
+
+(define (draw-state input) (place-image BOARD (/ HEIGHT 2) (/ WIDTH 2) SCENE))
 
 
 (big-bang 0
